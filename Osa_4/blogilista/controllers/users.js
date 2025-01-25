27bddd221a335d29870d5217dc26b364acbd8b24
Mikko Lipsanen <lipsanenm@gmail.com>
2025-01-25 23:05:14 +0200

    Tehtävä 4.17

--- Osa_4/blogilista/controllers/users.js
+++ Osa_4/blogilista/controllers/users.js
@@ -3,32 +3,32 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, id: 1 })
     response.json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+    const { username, name, password } = request.body
 
-  if (password === undefined) {
-    return response.status(400).json({ error: 'password missing' })
-  } 
-  if (password.length < 3) {
-    return response.status(400).json({ error: 'minimum length for password is 3 characters' })
-  }
+    if (password === undefined) {
+        return response.status(400).json({ error: 'password missing' })
+    } 
+    if (password.length < 3) {
+        return response.status(400).json({ error: 'minimum length for password is 3 characters' })
+    }
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const user = new User({
-    username,
-    name,
-    passwordHash
-  })
+    const user = new User({
+        username,
+        name,
+        passwordHash
+    })
 
-  const savedUser = await user.save()
+    const savedUser = await user.save()
 
-  response.status(201).json(savedUser)
+    response.status(201).json(savedUser)
 })
 
 
--- Osa_4/blogilista/models/user.js
+++ Osa_4/blogilista/models/user.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    minlength: 3,
-    unique: true
-  },
-  name: String,
-  passwordHash: String,
+    username: {
+        type: String,
+        required: true,
+        minlength: 3,
+        unique: true
+        },
+    name: String,
+    passwordHash: String,
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ]
 })
 
 userSchema.set('toJSON', {
@@ -16,7 +22,6 @@ userSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    // the passwordHash should not be revealed
     delete returnedObject.passwordHash
   }
 })
