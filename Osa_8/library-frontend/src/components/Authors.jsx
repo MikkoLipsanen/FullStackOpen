4086ac52081d7010e5d9d1c8bd2b303fcb932b02
Mikko Lipsanen <lipsanenm@gmail.com>
2025-02-21 15:12:26 +0200

    Tehtävät 8.8-8.12

---
+++ Osa_8/library-frontend/src/components/Authors.jsx
@@ -0,0 +1,73 @@
+import { useQuery } from '@apollo/client'
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_BIRTHYEAR } from '../queries'
+
+const Authors = ({ show }) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const authors = useQuery(ALL_AUTHORS)
+  const authorsWithoutBirthyear = authors.loading ? null : authors.data.allAuthors.filter(a => a.born === null)
+
+  const [ changeBirthyear ] = useMutation(EDIT_BIRTHYEAR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
+  
+  if (!show) {
+    return null
+  }
+
+  if (authors.loading)  {
+    return <div>loading...</div>
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+    const authorName = name === '' ? authorsWithoutBirthyear[0].name : name
+    changeBirthyear({  variables: { authorName, born } })
+
+    setName('')
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h2>Authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.data.allAuthors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <select value={name} onChange={({ target }) => setName(target.value)}>
+          {authorsWithoutBirthyear.map((a) => (
+            <option key={a.name} value={a.name}>{a.name}</option>
+          ))}
+        </select>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(parseInt(target.value))}
+          />
+        </div>
+        <button type="submit">Update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default Authors
